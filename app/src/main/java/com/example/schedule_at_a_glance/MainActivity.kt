a23895999c5341086756a7e9e020cf6bde0a42c9
200400481 <45043451+200400481@users.noreply.github.com>
2021-11-10 19:00:43 -0500

    Create activity for adding new tasks, set up view binding

--- app/src/main/java/com/example/schedule_at_a_glance/MainActivity.kt
+++ app/src/main/java/com/example/schedule_at_a_glance/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.schedule_at_a_glance
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.schedule_at_a_glance.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // On button press
+        binding.button.setOnClickListener {
+            // check if task name and category have been entered
+            if (binding.editTextTaskName.text.toString().isNotEmpty() &&
+                    binding.spinnerCategory.selectedItemPosition > 0)
+            {
+                // Add the task
+
+                // Default priority level to low if no selection is made
+            }
+            else
+            {
+                // Don't add the task
+            }
+        }
+
     }
 }
