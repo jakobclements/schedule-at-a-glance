fb94282094841331bddb6fd7ae11ae369c68ea89
200400481 <45043451+200400481@users.noreply.github.com>
2021-11-13 17:06:33 -0500

    Connect to FireStore DB, new tasks can be created and added to the db

--- app/src/main/java/com/example/schedule_at_a_glance/MainActivity.kt
+++ app/src/main/java/com/example/schedule_at_a_glance/MainActivity.kt
@@ -1,27 +1,62 @@
 package com.example.schedule_at_a_glance
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.RequiresApi
 import com.example.schedule_at_a_glance.databinding.ActivityMainBinding
+import com.google.firebase.firestore.FirebaseFirestore
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         // On button press
-        binding.button.setOnClickListener {
+        binding.buttonCreateTask.setOnClickListener {
             // check if task name and category have been entered
             if (binding.editTextTaskName.text.toString().isNotEmpty() &&
                     binding.spinnerCategory.selectedItemPosition > 0)
             {
                 // Add the task
+                val task = Task()
+                task.taskName = binding.editTextTaskName.text.toString()
+                task.category = binding.spinnerCategory.selectedItem.toString()
+                // Default importance level to low if no selection is made (pos 0)
+                if (binding.spinnerImportance.selectedItemPosition == 0)
+                {
+                    task.importance = binding.spinnerImportance.getItemAtPosition(1).toString()
+                }
+                else
+                {
+                    task.importance = binding.spinnerImportance.selectedItem.toString()
+                }
 
-                // Default priority level to low if no selection is made
+
+                // Date formatting
+                val dueDateYear = binding.datePickerDate.year
+                val dueDateMonth = binding.datePickerDate.month
+                val dueDateDay = binding.datePickerDate.dayOfMonth
+                val calendar = Calendar.getInstance()
+                calendar.set(dueDateYear, dueDateMonth, dueDateDay)
+                Log.d("Selected time", calendar.time.toString())
+                task.dueDate = calendar.time
+
+                // Get id from FireStore
+                val db = FirebaseFirestore.getInstance().collection("tasks")
+                task.id = db.document().id
+
+                // Create record
+                db.document(task.id!!).set(task)
+
+
             }
             else
             {
---
+++ app/src/main/java/com/example/schedule_at_a_glance/Task.kt
@@ -0,0 +1,10 @@
+package com.example.schedule_at_a_glance
+
+import java.util.*
+
+
+class Task (var id: String?=null,
+            var taskName: String?=null,
+            var category: String?=null,
+            var importance: String?=null,
+            var dueDate: Date?=null)
