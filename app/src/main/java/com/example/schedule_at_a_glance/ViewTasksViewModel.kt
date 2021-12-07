871caf0fa5f765266c518accfcb5d54dedbf97bb
200400481 <45043451+200400481@users.noreply.github.com>
2021-12-06 20:59:51 -0500

    Set up ViewModel for ViewTasksActivity

---
+++ app/src/main/java/com/example/schedule_at_a_glance/ViewTasksViewModel.kt
@@ -0,0 +1,66 @@
+package com.example.schedule_at_a_glance
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+
+class ViewTasksViewModel : ViewModel() {
+
+    // Mutable list to store Task objects
+    private val tasks = MutableLiveData<List<Task>>()
+
+    // Init viewmodel
+    init{
+        loadTasks()
+    }
+
+    // Loads Task objects from FireStore
+    private fun loadTasks()
+    {
+        // Connect to db, init query
+        val db = FirebaseFirestore.getInstance().collection("tasks")
+            .orderBy("dueDate", Query.Direction.ASCENDING)
+
+        // Create snapshot listener for tasks
+        db.addSnapshotListener{ documents, e ->
+            // Check for exception and log if found
+            e?.let {
+                Log.w("DB Response", "Listen failed.", e)
+                return@addSnapshotListener
+            }
+
+            // Snapshot listener connected successfully
+            Log.d("DB Response", "Current data: ${documents?.size()} elements found")
+
+            // ArrayList to hold Task objects from DB and populate MutableLiveData list
+            val taskList = ArrayList<Task>()
+
+            // Check if there are documents
+            documents?.let{
+                // Iterate through documents
+                for (document in documents)
+                {
+                    try {
+                        // Get Task object from db
+                        val task = document.toObject(Task::class.java)
+                        // Add task object to list
+                        taskList.add(task)
+                    }catch(e : Exception)
+                    {
+                        Log.e("Error", "Exception Thrown", e)
+                    }
+                }
+            }
+            tasks.value = taskList
+        }
+    }
+
+    fun getTasks() : LiveData<List<Task>>
+    {
+        return tasks
+    }
+
+}
