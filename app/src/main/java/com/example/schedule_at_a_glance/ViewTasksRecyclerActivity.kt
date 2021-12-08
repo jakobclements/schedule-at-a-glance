aa5c3eca7263bc7868683a9a3ad54b31aabf83c2
200400481 <45043451+200400481@users.noreply.github.com>
2021-12-08 16:07:03 -0500

    Implement grid layout for task list, add some styling, port buttons from old view to the RecyclerView

--- app/src/main/java/com/example/schedule_at_a_glance/ViewTasksRecyclerActivity.kt
+++ app/src/main/java/com/example/schedule_at_a_glance/ViewTasksRecyclerActivity.kt
@@ -1,17 +1,20 @@
 package com.example.schedule_at_a_glance
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
-import com.example.schedule_at_a_glance.databinding.ActivityRecyclerViewTasksBinding
+import com.example.schedule_at_a_glance.databinding.ActivityViewTasksRecyclerBinding
+import com.firebase.ui.auth.AuthUI
 
 class ViewTasksRecyclerActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityRecyclerViewTasksBinding
+    private lateinit var binding : ActivityViewTasksRecyclerBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityRecyclerViewTasksBinding.inflate(layoutInflater)
+        binding = ActivityViewTasksRecyclerBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val viewModel : ViewTasksViewModel by viewModels()
@@ -19,6 +22,25 @@ class ViewTasksRecyclerActivity : AppCompatActivity() {
             var recyclerViewAdapter = ViewTasksRecyclerViewAdapter(this, tasks)
             binding.viewTasksRecyclerView.adapter = recyclerViewAdapter
         })
+
+        // Add task button
+        binding.fabCreateTask.setOnClickListener{
+            startActivity(Intent(this, CreateTaskActivity::class.java))
+        }
+
+        // Logout button
+        binding.buttonLogout.setOnClickListener{
+            AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener {
+                    startActivity(Intent(this, LoginActivity::class.java))
+                }
+        }
+
+        // Options button
+        binding.buttonOptions.setOnClickListener{
+            Toast.makeText(this, "No options yet - check back soon!", Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
